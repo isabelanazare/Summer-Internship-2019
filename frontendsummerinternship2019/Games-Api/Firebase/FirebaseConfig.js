1d83e0efc72ab01fba04aa3cde5fb5b37ad27e2f
Isabela Nazare <41235452+isabelanazare@users.noreply.github.com>
2019-10-05 17:15:38 +0300

    Add files via upload

---
+++ frontendsummerinternship2019/Games-Api/Firebase/FirebaseConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "games-app-cd424.firebaseapp.com",
+    databaseURL: "https://games-app-cd424.firebaseio.com",
+    projectId: "games-app-cd424",
+    storageBucket: "games-app-cd424.appspot.com",
+    messagingSenderId: "454513976003",
+    appId: "1:454513976003:web:6bb3aed01ff8b5aa"
+};
